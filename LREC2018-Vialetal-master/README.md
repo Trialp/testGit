320e5ad36f7e24acf79ad1dde8e41fb3009783e0
Maximilien Faure <maximilien.faure38400@gmail.com>
2018-03-27 19:31:07 +0200

    Suppression des anciens fichiers

--- LREC2018-Vialetal-master/README.md
+++
@@ -1,3 +0,0 @@
-# UFSAC: Unification of Sense Annotated Corpora and Tools
-
-This repository has been moved to https://github.com/getalp/UFSAC
--- UFSAC-master/java/src/main/java/getalp/wsd/ufsac/converter/CorpusConverter.java
+++
@@ -1,6 +0,0 @@
-package getalp.wsd.ufsac.converter;
-
-public interface CorpusConverter
-{
-    public void convert(String originalCorpusPath, String newCorpusPath, int wordnetVersion);
-}
